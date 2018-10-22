57f5c376920b7598d4cca5e65b39d7479ebfb59f
David Refaeli <drefaeli@lightricks.com>
2018-10-22 20:29:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/drefaeli/myphotos/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.drefaeli.myphotos;
+
+import android.Manifest;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
